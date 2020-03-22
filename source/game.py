4493221efccd4f162d12d3a84617540be97635b5
Michal Piekarski <egzosted@gmail.com>
2020-03-22 18:09:51 +0100

    Structure changed, exceptions and points counting added

---
+++ source/game.py
@@ -0,0 +1,72 @@
+import random
+
+
+class Game:
+    def __init__(self):
+        self.words = self.read_words()
+        self.mistakes = []
+        self.points = 0
+
+    def read_words(self):
+        """
+        Creating list of words from given file
+        param file_name: path to file that you want to read
+        """
+        read = False
+        while(not read):
+            file_name = input('Enter path to your file: ')
+            try:
+                with open(file_name, 'r'):
+                    words = [line.rstrip('\n')
+                             for line in open(file_name)]  # list of words from file
+                    read = True
+            except FileNotFoundError:
+                print("File doesn't exist. Press y to enter new path")
+                ans = input()
+                if ans == 'y':
+                    read = False
+                else:
+                    exit()
+        return words
+
+    def mix_order(self):
+        """
+        Changing order of words in list
+        param words: list of words with following order (english\npolish\n etc)
+        """
+        for i in range(len(self.words) // 2):
+            j = random.randint(0, len(self.words) // 2 - 1)
+            self.words[2 * i], self.words[2 * j] = self.words[2 * j], self.words[2 * i]
+            self.words[2 * i + 1], self.words[2 * j +
+                                              1] = self.words[2 * j + 1], self.words[2 * i + 1]
+
+    def play(self):
+        """
+        Checking if user knows vocabulary
+        """
+        for i in range(len(self.words) // 2):
+            correct = self.words[2 * i]
+            word = self.words[2 * i + 1]
+            print(word)
+            answer = input()
+            if answer == correct:
+                self.points += 1
+                print('Ok')
+            else:
+                print(f'Mistake. Correct answer is {correct}')
+                self.mistakes.append(correct)
+                self.mistakes.append(word)
+            print()
+
+    def summarize(self):
+        """
+        Printing mistakes and correct translation for these read_words
+        """
+        print(f'Your result {self.points} / {len(self.words) // 2}\n')
+        if len(self.mistakes) == 0:
+            print('Good job')
+        else:
+            print('Your mistakes: ')
+            for i in range(len(self.mistakes) // 2):
+                print(f'{self.mistakes[2 * i]} {self.mistakes[2 * i + 1]}')
+        print()
--- source/main.py
+++ source/main.py
@@ -1,34 +1,26 @@
 # basic vocabulary checker, we have to translate word from polish to english
 # user types name of file, program loads words and then until last word user has to answer
-import random
+from game import Game
+import os
 
+end = ' '
 
-file_name = input('Enter file name ')
-with open(file_name, 'r'):
-    words = [line.rstrip('\n') for line in open(file_name)]  # list of words
+while(end != 'q'):
+    os.system('clear')
 
-for i in range(len(words) // 2):
-    j = random.randint(0, len(words) // 2 - 1)
-    words[2 * i], words[2 * j] = words[2 * j], words[2 * i]
-    words[2 * i + 1], words[2 * j + 1] = words[2 * j + 1], words[2 * i + 1]
+    game = Game()
 
-mistakes = []
-for i in range(len(words) // 2):
-    english = words[2 * i]
-    polish = words[2 * i + 1]
-    print(polish)
-    answer = input()
-    if answer == english:
-        print('Ok')
-    else:
-        print(f'Mistake. Correct answer is {english}')
-        mistakes.append(english)
-        mistakes.append(polish)
-    print()
+    print('Do you want random order of words [y/n]? ', end=' ')
+    random_order = input().lower()
 
-if len(mistakes) == 0:
-    print('Good job')
-else:
-    print('Your mistakes: ')
-    for i in range(len(mistakes) // 2):
-        print(f'{mistakes[2 * i]} {mistakes[2 * i + 1]}')
+    if random_order == 'y':
+        game.mix_order()
+    elif random_order != 'n':
+        print('Incorrect value. Game over.')
+        end = 'q'
+        continue
+
+    game.play()
+    game.summarize()
+    print('Press q to quit, different value will start new game')
+    end = input()
